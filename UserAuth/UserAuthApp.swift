09b07718f98b4a8871a975104f15dca376f28c57
bryson-evans <bryson.evans@hgs.hiddengeinusproject.org>
2025-06-30 14:16:02 -0400

    Initial Commit

---
+++ UserAuth/UserAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthApp.swift
+//  UserAuth
+//
+//  Created by bryson evans on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct UserAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserAuthTests/UserAuthTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthTests.swift
+//  UserAuthTests
+//
+//  Created by bryson evans on 6/30/25.
+//
+
+import Testing
+@testable import UserAuth
+
+struct UserAuthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
